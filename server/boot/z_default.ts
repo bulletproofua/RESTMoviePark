1ebeffb9b80939b563d1df690f9b9105c2aed25b
bulletproofua <yuriy.haydeychuk@gmail.com>
2017-10-01 12:32:52 +0300

    login

---
+++ server/boot/z_default.ts
@@ -0,0 +1,25 @@
+function setDefaults(server:any, next: any) {
+	(async () => {
+		await server.models.Role.create({
+			name: 'ADMIN',
+		});
+		await server.models.User.create({
+			username: 'admin',
+			password: 'admin',
+			email: 'admin@bar.com',
+		});
+		await server.models.RoleMapping.create({
+			roleId: '1',
+			principalId: '1',
+			principalType: 'USER',
+		});
+		await server.models.AccessToken.create({
+			id: '123456',
+			userId: '1',
+		})
+	})().then(next).catch((err:any)=>{
+			console.log(err);
+			throw err;
+	});
+}
+export = setDefaults;
